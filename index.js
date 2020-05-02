6c077b076368b37a6ab84c4842bcadd980eaa636
Alireza Azimi <sazimi@ualberta.ca>
2020-05-01 20:53:06 -0600

    official endpoint

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const querystring = require('querystring');
 const app = express();
 app.use(express.static('public'));
 //q4QbxT9vVV5WW6DO
-mongoose.connect("mongodb+srv://admin:q4QbxT9vVV5WW6DO@cluster0-hc4da.mongodb.net/testDB", {
+mongoose.connect("mongodb+srv://admin:q4QbxT9vVV5WW6DO@cluster0-hc4da.mongodb.net/SCISAT_Data", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
@@ -24,6 +24,24 @@ pointSchema = {
   y: Number
 };
 
+concentrationSchema = {
+  _id: mongoose.Schema.ObjectId,
+  occultation_name: [String],
+  event_type: [String],
+  date: [String],
+  date_MJD2000: [String],
+  latitude: [String],
+  longitude: [String],
+  beta_angle: [String],
+  start_timetag: [String],
+  end_timetag: [String],
+  start_time: [String],
+  end_time: [String],
+  molecule_range: [String],
+  molecule_name: [String],
+  alt_conc: [[mongoose.Schema.Number]]
+}
+
 function insertTest(){
   for (let i = 0;i < 5000; i++ ){
     const point = new Point({
@@ -37,6 +55,7 @@ function insertTest(){
 
 const Test = mongoose.model("Test", testSchema);
 const Point = mongoose.model("Point", pointSchema);
+const Ozone = mongoose.model("TimePeriod", concentrationSchema, "TimePeriod");
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -70,6 +89,15 @@ app.route("/api/:paramName")
           res.send(err);
         } else {
           res.send(foundItems);
+        }
+      });
+    } else if(collection === "Data"){
+      Ozone.find({date:{$regex:"2004-03.*"}}, (err, foundItems)=>{
+        if (err){
+          res.send(err);
+        } else {
+          console.log(foundItems);
+          res.send({Data:foundItems});
         }
       });
     }
